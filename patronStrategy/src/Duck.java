ef8f8cea35477961ba37de4bfd0926684c6a6788
Josue-cloudU <75402445+Josue-cloudU@users.noreply.github.com>
2022-03-16 20:24:47 -0700

    strategy

---
+++ patronStrategy/src/Duck.java
@@ -0,0 +1,28 @@
+public abstract class Duck {
+
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+    public Duck() { }
+
+    public abstract void display();
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+//    public void setFlyBehavior(FlyBehavior fly) {
+//        flyBehavior = fly;
+//    }
+//
+//    public void setQuackBehavior(QuackBehavior quack) {
+//        quackBehavior = quack;
+//    }
+
+    public void swim(){
+        System.out.println("All ducks float, even decoys!");
+    }
+}
